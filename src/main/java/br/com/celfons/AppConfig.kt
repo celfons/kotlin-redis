0cccb77d8609451abba21b32e4966b4549fad96a
marcelresendezup <marcel.resende@zup.com.br>
2020-07-02 22:09:47 -0300

    fix

--- src/main/java/br/com/celfons/AppConfig.kt
+++ src/main/java/br/com/celfons/AppConfig.kt
@@ -1,27 +1,20 @@
 package br.com.celfons
 
-import br.com.celfons.controllers.responses.MessageResponse
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
 import org.springframework.data.redis.cache.RedisCacheConfiguration
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair
-
 
 @SpringBootApplication
 @EnableCaching
 open class AppConfig {
     @Bean
-    @Primary
-    open fun defaultCacheConfig(): RedisCacheConfiguration? {
-        return RedisCacheConfiguration.defaultCacheConfig()
+    open fun defaultCacheConfig(): RedisCacheConfiguration? = RedisCacheConfiguration.defaultCacheConfig()
             .serializeValuesWith(SerializationPair.fromSerializer(JdkSerializationRedisSerializer()))
-    }
 }
 
 fun main(args: Array<String>) {
